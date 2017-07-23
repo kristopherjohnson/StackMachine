37e5d46e019c9d48245fd1c66ac35c2df9d0b516
Kristopher Johnson <kris@kristopherjohnson.net>
2017-07-23 10:22:38 -0400

    Initial Commit

---
+++ StackMachine/StackMachine.h
@@ -0,0 +1,19 @@
+//
+//  StackMachine.h
+//  StackMachine
+//
+//  Created by Kristopher Johnson on 7/23/17.
+//  Copyright © 2017 Kristopher Johnson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StackMachine.
+FOUNDATION_EXPORT double StackMachineVersionNumber;
+
+//! Project version string for StackMachine.
+FOUNDATION_EXPORT const unsigned char StackMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackMachine/PublicHeader.h>
+
+
